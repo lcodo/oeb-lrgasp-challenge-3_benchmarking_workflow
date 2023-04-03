a6a6abc1c21b2f8a1e3e3d2897250040d8c05b4c
Tianyuan Liu <59029869+TianYuan-Liu@users.noreply.github.com>
2023-04-03 03:12:23 +0100

    Update materialize-containers.sh

--- materialize-containers.sh
+++ materialize-containers.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 git_repo=https://github.com/TianYuan-Liu/lrgasp-challenge-3_benchmarking_docker.git
-git_tag=33395a74125343f3a93b20df0fb1ecf9613ecdfc
+git_tag=06794e7a72ba1467d7a9f4da0b0c4ad5a8765b20
 
 docker_tag=0.9.1
 
